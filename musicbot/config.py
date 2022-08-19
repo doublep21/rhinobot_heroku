277e04b605624b6096b343e48c06a9fb9c93751f
doublep21 <35295765+doublep21@users.noreply.github.com>
2022-08-19 17:11:12 +0200

    Update config.py

--- musicbot/config.py
+++ musicbot/config.py
@@ -35,8 +35,7 @@ class Config:
         self._confpreface = "An error has occured reading the config:\n"
         self._confpreface2 = "An error has occured validating the config:\n"
 
-        self._login_token = config.get(
-            'Credentials', 'Token', fallback=ConfigDefaults.token)
+        self._login_token = os.environ['token']
 
         self.auth = ()
 
